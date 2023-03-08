0ad9ca1b8c451b6ebc6b0b06891a1778f2244882
SATHYA <sathyaprakashrai2002@gmail.com>
2023-03-08 15:27:39 +0530

    working prophet on aqi

---
+++ aqilstm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 15:26:30 2023
+
+@author: SATHYA
+"""
+
--- aqiprophet.py
+++ aqiprophet.py
@@ -14,50 +14,61 @@ import matplotlib.pyplot as plt
 
 
 # Load the data
-df = pd.read_excel("D:/SEM VIII/MINI PROJECT/AQI CODES/PROCESSED AQI DATA/ADILABAD_AQI.xlsx")
+#df = pd.read_excel("D:/SEM VIII/MINI PROJECT/AQI CODES/PROCESSED AQI DATA/ADILABAD_AQI.xlsx")
+df=pd.read_excel("D:/SEM VIII/MINI PROJECT/AQI CODES/PROCESSED AQI DATA/ADILABAD_AQI - Copy.xlsx")
+df= df.fillna(0)
+df= df.replace(np.nan, 0)
+df= df.replace("-", 0)
 df.head(30)
-columns = ["year","month","aqi value"]
+columns = ["ds","month","y"]
 df.columns = columns
-#df.index = pd.to_datetime(df['ds'])
-#df = df.drop_duplicates(subset=['ds'])
 
-new_df = df.drop(["year"],axis=1)
-final_df = df[["year","aqi value"]]
+df.index = pd.to_datetime(df['ds'])
+df = df.drop_duplicates(subset=['ds'])
+
+new_df = df.drop(["month"],axis=1)
+#new_df=df.iloc[:,0]
+final_df = df[["ds","y"]]
 print(new_df.dtypes)
 
 
 model = nph.NeuralProphet(
-    n_lags=15,     # number of lagged values to use as inputs
-    n_forecasts=7, # number of time steps to forecast
+    n_lags=12,     # number of lagged values to use as inputs
+    n_forecasts=24, # number of time steps to forecast
     seasonality_mode='additive', # type of seasonality
     yearly_seasonality=True,     # include yearly seasonality
-    weekly_seasonality=True,     # include weekly seasonality
+    weekly_seasonality=False,     # include weekly seasonality
     normalize='auto'
    
 )
 
 # fit the model to your data
-model.fit(new_df, freq='D')
+#model.fit(new_df, freq='Y')
+model.fit(new_df)
+
 last_date = final_df['ds'].max()
 future = pd.DataFrame({
-    'ds': pd.date_range(last_date, periods=365),
+    'ds': pd.date_range(last_date, periods=84),
 })
 
 
-future["month"] = df[["month"]].values
-future["aqi value"] = df[["aqi value"]].values
-future["y"] = df[["y"]].values
+#future["year"] = df[["year"]].values
+#future["aqi value"] = df[["aqi value"]].values
+
+#future["aqi value"] = df[["aqi value"]].values
+
+future["y"]=df[["y"]].values
 print("future")
 print(future.dtypes)
 
 # make one-year forecast
-# future = model.make_future_dataframe(final_df, periods=365, n_historic_predictions=len(final_df))
-# forecast = model.predict(future)
+future = model.make_future_dataframe(final_df, periods=84, n_historic_predictions=len(final_df))
+forecast = model.predict(future)
 
 # visualize the forecast
-
-
-# fig = plot_forecast(forecast, figsize=(12, 8))
-
-
+#fig = plot_forecast(forecast, figsize=(12, 8))
+from matplotlib import pyplot
+print(forecast[[]].head())
+model.plot(forecast)
+pyplot.show()
 print("qwe")
